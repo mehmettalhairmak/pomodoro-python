fb9d699f76324fb10e588d78cfd0df19237836e6
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2024-04-13 19:32:35 +0300

    added time reset functions

--- main.py
+++ main.py
@@ -10,15 +10,43 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it's the 8th rep:
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    # If it's 2nd/4th/6th rep:
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    # If it's the 1st/3rd/5th/7th rep:
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -33,7 +61,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            mark += "✓"
+        check_marks.config(text=mark)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -54,7 +90,7 @@ start_button = Button(text="Start", highlightthickness=0, borderwidth=0, highlig
                       command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0, borderwidth=0, highlightbackground=YELLOW)
+reset_button = Button(text="Reset", highlightthickness=0, borderwidth=0, highlightbackground=YELLOW, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
